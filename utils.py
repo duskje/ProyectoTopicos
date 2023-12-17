befc1c9054154fb62a73e190d558a15d88adef62
duskje <duskje@protonmail.com>
2023-12-16 21:42:01 -0300

    improvement: easy way to calculate leading zeros

---
+++ utils.py
@@ -0,0 +1,37 @@
+import string
+import random
+
+import mmh3
+
+
+def generate_random_car_plate(exclude):
+    result = ''
+
+    while not result or result in exclude:
+        result += str(random.choice(string.ascii_uppercase))
+        result += str(random.choice(string.ascii_uppercase))
+        result += '-'
+        result += str(random.choice(string.ascii_uppercase))
+        result += str(random.choice(string.ascii_uppercase))
+        result += '-'
+        result += str(random.choice(list(range(10))))
+        result += str(random.choice(list(range(10))))
+
+    return result
+
+
+def find_leading_zeros_for_hll(value: int, p: int):
+    count = 0
+
+    for bit_idx in reversed(range(64 - p)):
+        if value & (1 << bit_idx):
+            return count
+
+        count += 1
+
+    return count
+
+if __name__ == '__main__':
+    h = mmh3.hash64('AA-AA-AA', 1, False)[0]
+    ldz = find_leading_zeros_for_hll(value=h, p=14)
+    print(ldz)
